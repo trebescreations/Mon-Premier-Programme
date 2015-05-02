1755a345dd7dc91a4f8c4f518ac96f9f8ce3003d
Florian TREBES <floriantrebes@tflorian.home>
2015-05-02 11:05:17 +0400

    Initial Commit

---
+++ Mon Premier Programme/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Mon Premier Programme
+//
+//  Created by Florian TREBES on 02/05/2015.
+//  Copyright (c) 2015 TREBES. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
